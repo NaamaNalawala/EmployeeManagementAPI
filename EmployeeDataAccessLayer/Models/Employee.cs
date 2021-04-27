8e73aa76724f490741d239fc92031da0c3254698
naama.nalawala <naama.nalawala@gmail.com>
2021-04-27 18:17:19 +0530

    change in employee model

--- EmployeeDataAccessLayer/Models/Employee.cs
+++ EmployeeDataAccessLayer/Models/Employee.cs
@@ -15,10 +15,20 @@ namespace EmployeeDataAccessLayer.Models
         public DateTime? ModifiedDt { get; set; }
         public string CreatedBy { get; set; }
         public string ModifiedBy { get; set; }
-        public EmployeeProfile EmployeeProfile { get; set; }
         public string Role { get; set; }
         public string Designation { get; set; }
-
+        public Guid ProfileId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string EmailId { get; set; }
+        public long ContactNum { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Education { get; set; }
+        public string UserImage { get; set; }
 
 
     }
